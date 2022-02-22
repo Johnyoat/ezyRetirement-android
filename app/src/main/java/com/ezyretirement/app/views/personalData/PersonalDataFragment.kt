66aaafd01aa37eb7b3666ae9fe7f2e9ab2994144
Johnson Osei Yeboah <johnyoat@gmail.com>
2022-02-22 13:36:09 -0600

    feat:personal data fragment

---
+++ app/src/main/java/com/ezyretirement/app/views/personalData/PersonalDataFragment.kt
@@ -0,0 +1,34 @@
+package com.ezyretirement.app.views.personalData
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.ezyretirement.app.R
+import com.ezyretirement.app.databinding.PersonalDataFragmentBinding
+
+class PersonalDataFragment : Fragment() {
+    private val viewModel by viewModels<PersonalDataViewModel>()
+    private lateinit var binding: PersonalDataFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = PersonalDataFragmentBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.title.text = getString(R.string.personal_data)
+        binding.toolbar.title.setTextColor(ContextCompat.getColor(requireContext(),R.color.white))
+        binding.toolbar.backBtn.setColorFilter(ContextCompat.getColor(requireContext(),R.color.white))
+    }
+
+}
